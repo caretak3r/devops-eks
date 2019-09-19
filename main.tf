288285304e7c54c66a595cc4612e5400e1f34681
Rohit Gudi <rgudi@cadent.tv>
2019-09-19 10:29:41 -0400

    refactor: Map devops user and create default kubeconfig

--- main.tf
+++ main.tf
@@ -5,6 +5,11 @@ module "eks" {
   subnets = data.aws_subnet_ids.private.ids
   vpc_id = data.aws_vpc.shared-vpc.id
 
+  tags = {
+    Environment = "shared-services"
+    GithubRepo = "CadentTech/devops-eks"
+  }
+
   worker_groups = [
     {
       name = "${var.prefix}-worker-ng"
@@ -12,9 +17,12 @@ module "eks" {
       iam_instance_profile_name = aws_iam_instance_profile.node.name
 
       autoscaling_enabled = true
+      protect_from_scale_in = true
       asg_desired_capacity = "2"
       asg_max_size = "3"
       asg_min_size = "1"
+
+      ebs_optimized = false
 
       key_name = "greenfield"
 
@@ -35,4 +43,10 @@ module "eks" {
   cluster_iam_role_name = aws_iam_role.ServiceRole.name
   cluster_log_retention_in_days = 7
   cluster_security_group_id = aws_security_group.AmazonEKSClusterSecurityGroup.id
+
+  map_users = var.map_users
+
+  manage_aws_auth = true
+  write_kubeconfig = true #
+  config_output_path = "/root/.kube/"
 }
