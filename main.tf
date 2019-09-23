cf314264841cdf8a3f81c740383db49140e37e27
Rohit Gudi <rgudi@cadent.tv>
2019-09-23 14:06:12 -0400

    refactor: Add todos to install cluster-autoscaler

--- main.tf
+++ main.tf
@@ -15,10 +15,10 @@ module "eks" {
       name = "${var.prefix}-worker-ng"
       instance_type = "m5.large"
       iam_instance_profile_name = aws_iam_instance_profile.node.name
-      autoscaling_enabled = true
-      protect_from_scale_in = true
-      asg_desired_capacity = "2"
-      asg_max_size = "3"
+      autoscaling_enabled = false #todo: enable this when CA-autoscaler is ready
+      protect_from_scale_in = false #todo: enable this when CA-autoscaler is ready
+      asg_desired_capacity = "1" # currently it is being ignored in terraform to reduce complexity
+      asg_max_size = "1"
       asg_min_size = "1"
 
       ebs_optimized = false
@@ -38,13 +38,13 @@ module "eks" {
   worker_security_group_id = aws_security_group.AmazonEKSWorkerNodeSecurityGroup.id
   cluster_enabled_log_types = ["api", "audit", "authenticator", "controllerManager", "scheduler"]
   cluster_endpoint_private_access = true
-  cluster_endpoint_public_access = false
+  cluster_endpoint_public_access = true
   cluster_iam_role_name = aws_iam_role.ServiceRole.name
   cluster_log_retention_in_days = 7
   cluster_security_group_id = aws_security_group.AmazonEKSClusterSecurityGroup.id
 
   map_users = var.map_users
   manage_aws_auth = true
-  write_kubeconfig = true #
+  write_kubeconfig = true
   config_output_path = "/root/.kube/"
 }
